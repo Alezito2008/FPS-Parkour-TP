24ffa4d1e518aaf7394092cc331e45d493dbd375
Alezito <49090854@ort>
2025-05-26 09:59:51 -0300

    Mostrar puntos

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score = 0;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -15,5 +17,7 @@ public class ScoreManager : MonoBehaviour
     public void addScore()
     {
         score++;
+        Debug.Log(score);
+        scoreText.text = $"Score: {score}";
     }
 }
