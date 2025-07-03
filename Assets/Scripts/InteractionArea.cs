b68e75edccdc890d8d406d4d98765da285097017
Alezito2008 <>
2025-07-03 10:41:05 -0300

    Interactuar

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject UIInteractionMessage;
+    public GameObject currentInteractable = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Interactable")) return;
+        UIInteractionMessage.SetActive(true);
+        currentInteractable = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Interactable")) return;
+        UIInteractionMessage.SetActive(false);
+        if (currentInteractable == other.gameObject) currentInteractable = null;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            Destroy(currentInteractable);
+            currentInteractable = null;
+            UIInteractionMessage.SetActive(false);
+        }
+    }
+}
